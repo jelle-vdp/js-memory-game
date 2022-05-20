9a1080fcd7d1a2debde01a9af4f8883e4459732c
jelle-vdp <40336977+jelle-vdp@users.noreply.github.com>
2022-05-20 15:44:46 +0200

    added functionality to submit your own images

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const cardWrapper = document.querySelector(".cards-wrapper");
+const urlInput = document.querySelector(".add-images input");
+const urlSubmit = document.querySelector(".add-images button");
+const formErrorMsg = document.querySelector(".add-images__error");
+const formDoubleMsg = document.querySelector(".add-images__double");
+const formSuccessMsg = document.querySelector(".add-images__success");
+
 const urlArr = [
     "img/1.jpg",
     "img/2.jpg",
@@ -13,58 +20,83 @@ const urlArr = [
     "img/12.jpg"
 ];
 
-const cardWrapper = document.querySelector(".cards-wrapper");
+const isImg = url => /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 
-let cardsCount = 0;
-let firstCardIndex;
-let cardsMatched = 0;
+const generateGame = (imgArr) => {
 
-const shuffleArr = arr => {
-    let i = arr.length;
-    while (--i > 0) {
-      let randIndex = Math.floor(Math.random() * (i + 1));
-      [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
-    }
-    return arr;
+    let cardsCount = 0;
+    let firstCardIndex;
+    let cardsMatched = 0;
+    
+    const shuffleArr = arr => {
+        let i = arr.length;
+        while (--i > 0) {
+          let randIndex = Math.floor(Math.random() * (i + 1));
+          [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
+        }
+        return arr;
+    };
+    
+    const finalUrlArr = shuffleArr(imgArr.concat(imgArr));
+    
+    finalUrlArr.forEach(url => {
+        const el = `<div class="card"><div class="card__front"><i class="fa-solid fa-repeat"></i></div><div class=card__back><img src="${url}"></div></div>`;
+        cardWrapper.innerHTML = cardWrapper.innerHTML + el;
+    });
+    
+    const cards = document.querySelectorAll(".card");
+    
+    cards.forEach((card, index) => {
+        card.addEventListener("click", () => {
+            if (cardsCount !== 2 && !card.classList.contains("card--spin")){
+                card.classList.add("card--spin");
+                cardsCount++;
+                if (cardsCount === 1){
+                    firstCardIndex = index;
+                }
+        
+                if (cardsCount === 2){
+    
+                    if (card.querySelector("img").src !== cards[firstCardIndex].querySelector("img").src){
+                        
+                        setTimeout(() => {
+                            cards[firstCardIndex].classList.remove("card--spin");
+                            card.classList.remove("card--spin");
+                            cardsCount = 0;
+                        }, 1000);
+                    } else {
+                        cardsMatched++;
+                        setTimeout(() => cardsCount = 0, 1000);
+                    }
+    
+                    if (cardsMatched === urlArr.length){
+                        document.querySelector(".won").style.display = "flex";
+                    }
+                }
+            }
+        })
+    })
 };
 
-const finalUrlArr = shuffleArr(urlArr.concat(urlArr));
+generateGame(urlArr);
 
-finalUrlArr.forEach(url => {
-    const el = `<div class="card"><div class="card__front"><i class="fa-solid fa-repeat"></i></div><div class=card__back><img src="${url}"></div></div>`;
-    cardWrapper.innerHTML = cardWrapper.innerHTML + el;
-});
-
-const cards = document.querySelectorAll(".card");
-
-cards.forEach((card, index) => {
-    card.addEventListener("click", () => {
-        if (cardsCount !== 2 && !card.classList.contains("card--spin")){
-            card.classList.add("card--spin");
-            cardsCount++;
-            if (cardsCount === 1){
-                firstCardIndex = index;
-            }
-    
-            if (cardsCount === 2){
-
-                if (card.querySelector("img").src !== cards[firstCardIndex].querySelector("img").src){
-                    
-                    setTimeout(() => {
-                        cards[firstCardIndex].classList.remove("card--spin");
-                        card.classList.remove("card--spin");
-                        cardsCount = 0;
-                    }, 1000);
-                } else {
-                    cardsMatched++;
-                    setTimeout(() => cardsCount = 0, 1000);
-                }
-
-                if (cardsMatched === urlArr.length){
-                    document.querySelector(".won").style.display = "flex";
-                }
-            }
-        }
-    })
+urlSubmit.addEventListener("click", () => {
+    if (!isImg(urlInput.value)){
+        formErrorMsg.style.display = "block";
+        setTimeout(() => formErrorMsg.style.display = "none", 3000);
+    } else if (urlArr.includes(urlInput.value)) {
+        formDoubleMsg.style.display = "block";
+        setTimeout(() => formDoubleMsg.style.display = "none", 3000);
+    } else {
+        formErrorMsg.style.display = "none";
+        cardWrapper.innerHTML = "";
+        urlArr.pop();
+        urlArr.unshift(urlInput.value);
+        generateGame(urlArr);
+        urlInput.value = "";
+        formSuccessMsg.style.display = "block";
+        setTimeout(() => formSuccessMsg.style.display = "none", 3000);
+    }
 })
+
  
